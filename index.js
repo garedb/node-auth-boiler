0b56c6049a182cb054ca7d8db9c2a5db24060e62
garedb <garedbarney@gmail.com>
2020-04-13 10:33:28 -0700

    Implemented home page and created nav bar content

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+/*****************************
+* NODE MODULES
+/*****************************/
+
 // Require needed modules
 let express = require('express')
 let layouts = require('express-ejs-layouts')
 
 // Create an app instance
 let app = express()
+
+/*****************************
+* SETTING / MIDDLEWARE
+/*****************************/
 
 // Set template lang to EJS
 app.set('view engine', 'ejs')
@@ -14,11 +22,23 @@ app.use(layouts)
 // Set up the static folder
 app.use(express.static('static'))
 
+/*****************************
+* ROUTES
+/*****************************/
+// Creat a home page route above wildcard route
+app.get('/', (req, res) => {
+	res.render('home')
+})
+
 // Create a wildcard (catch-all) route
 app.get('*', (req, res) => {
 	res.render('error')
 
 })
+
+/*****************************
+* LISTEN
+/*****************************/
 
 // Pick a port to listen on
 app.listen(3000)
