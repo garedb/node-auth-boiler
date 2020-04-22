99d9c07d6b80a0e72c24a321c100803ebb91d424
garedb <garedbarney@gmail.com>
2020-04-22 12:36:09 -0700

    signup form implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@
 let router = require('express').Router()
 
 // Routes
+// GET /auth/login - this is a page that renders the login form
 router.get('/login', (req, res) => {
 	res.render('auth/login')
 })
@@ -12,8 +13,24 @@ router.post('/login', (req, res) => {
 	res.send('hello from the post route')
 })
 
+// GET /auth/signup - this is a page that renders the signup form
 router.get('/signup', (req, res) => {
-	res.render('auth/signup')
+	res.render('auth/signup', { data: {} })
+})
+
+// POST /auth/signup
+router.post('/signup', (req, res) => {
+	console.log('REQUEST BODY', req.body)
+	if (req.body.password !== req.body.password_verify) {
+		// res.send('PASSWORD MISMATCH')
+		// Send a message on why things didnt work
+		// Put the user back onto the signup form to try again
+		res.render('auth/signup', { data: req.body })
+	// }
+	// else {
+	// 	res.send('POST route reached - passwords are good!')
+	}
+	
 })
 
 // Export (allow me to include this in another page)
